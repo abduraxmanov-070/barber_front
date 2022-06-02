a0ed23da71294a4f8576e7fe573f1acca2066b0c
Lenovo <asadbek.polatov.02@mail.ru>
2022-06-02 07:47:28 +0500

    Stil qoshildi

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -54,29 +54,15 @@
     <section >
       <div class="container">
         <ul class="cards">
-          <li>
+          <li v-for="item in photos" v-bind:key="item.id">
             <a href="" class="card">
               <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
               <div class="card__overlay">
                 <div class="card__header">
                   <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
-                  <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+<!--                  <img class="card__thumb" src="http://barber.amusoft.uz/photo/" alt="" />-->
                   <div class="card__header-text">
-                    <h3 class="card__title">Jessica Parker</h3>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="" class="card">
-              <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
-              <div class="card__overlay">
-                <div class="card__header">
-                  <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
-                  <img class="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
-                  <div class="card__header-text">
-                    <h3 class="card__title">kim Cattrall</h3>
+                    <h3 class="card__title">{{ item.name}}</h3>
                   </div>
                 </div>
               </div>
@@ -90,44 +76,24 @@
     <section class="pb-lg-5">
       <div class="container">
         <div class="row">
-          <div class="col-md-6 ">
+          <div class="col-md-6 " v-for="item in videos" v-bind:key="item.id">
             <div class="card" >
               <div class="card-image">
                 <div class="embed-responsive embed-responsive-16by9">
-                  <iframe width="280" height="157" src="https://www.youtube.com/embed/DQ6Cjq6diLk" frameborder="0" allowfullscreen></iframe>
+                  <iframe width="280" height="157" :src="item['url']" frameborder="0" allowfullscreen></iframe>
                   <div class="content">
-                    <h1>The Timbers</h1>
+                    <h1>{{ item['name ']}}</h1>
                   </div>
                 </div>
               </div>
               <!-- card image -->
 
               <div class="card-content">
-                <span class="card-title">Thumping Rhythms</span>
-                <p>THE TIMBERS’ thumping rhythms and big misty mountain melodies will  </p>
+                <span class="card-title">{{ item.name }}</span>
+<!--                <p>THE TIMBERS’ thumping rhythms and big misty mountain melodies will  </p>-->
               </div>
               <!-- card content -->
 
-            </div>
-          </div>
-          <div class="col-md-6 ">
-            <div class="card">
-              <div class="card-image">
-                <div class="embed-responsive embed-responsive-16by9">
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/LR6xXs4lDsA" frameborder="0" allowfullscreen></iframe>
-                  <div class="content">
-                    <h1>Z-STAR Trinity</h1>
-
-                  </div>
-                </div>
-
-              </div>
-              <!-- card image -->
-              <div class="card-content">
-                <span class="card-title">Exhilarating to Watch</span>
-                <p>Shaking up the Australian blues scene, award-winning international artist  </p>
-              </div>
-              <!-- card content -->
             </div>
           </div>
         </div>
@@ -149,7 +115,7 @@ export default {
     return {photos: undefined , videos: undefined}
   },
   mounted() {
-    Vue.axios.get('http://barber.amusoft.uz/api/photoss')
+    Vue.axios.get('http://barber.amusoft.uz/api/photos')
       .then((resp) => {
         this.photos = resp.data;
         console.warn(resp.data)
